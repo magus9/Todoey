96cde152dbff467bc412a73b3f6615fabc9347fc
GUSTAFSSON MATS <matsgustafsson@gustafssons-macbook-pro.local>
2018-12-25 09:52:51 +0100

    Add Category.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by GUSTAFSSON MATS on 2018-12-24.
+//  Copyright © 2018 GUSTAFSSON MATS. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    //wait with this ..
+}
